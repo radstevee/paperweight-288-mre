c6e3a922fe11c19b01630b216a48d2cd4e9042c2
Nils Krüger <radsteve@radsteve.net>
2025-03-21 21:53:46 +0100

    add commented repo block

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
   id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
 }
 
+// Works when uncommenting this
+/* repositories {
+  mavenCentral()
+  maven("https://maven.radsteve.net/public")
+  maven("https://repo.papermc.io/repository/maven-public")
+} */
+
 dependencies {
   // Hosted on maven.radsteve.net, should be fetched from the
   // repos defined in the depResMgmt repo block
